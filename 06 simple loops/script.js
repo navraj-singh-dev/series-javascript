6f930c91847a9d531490f29207bf7043e6477307
Navraj Singh <148690973+navraj-singh-dev@users.noreply.github.com>
2023-11-06 11:41:53 +0530

    create and accomplish tasks of folder : 06 simple loops

---
+++ 06 simple loops/script.js
@@ -0,0 +1,18 @@
+// 1. for loop (use when you know the number of iterations)
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// 2. while loop (use when you know a condition, but not number of iterations)
+let i = 1;
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+// 3. do while loop
+let j = 10
+do {
+    console.log(j)
+    j++
+} while (j <= 5)
