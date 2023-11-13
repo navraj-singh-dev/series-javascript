878c5331ea9eddd662d9e20c12ed7ecec02c325a
Navraj Singh <148690973+navraj-singh-dev@users.noreply.github.com>
2023-11-13 08:43:28 +0530

    create and accomplish tasks of folder : 12 introduction - DOM

---
+++ 12 introduction - DOM/script.js
@@ -0,0 +1,24 @@
+// DOM (Document Object Model)
+    // Html file is called a document in browser and
+    // its individual tags and elements are called nodes and can be,
+    // considered as javascript objects.
+    // Browser makes a tree like structure for this html elements and tags
+    // and this document can be completely manipulated using JavaScript.
+
+// Window Object
+    // 1. A Browser's entire opened window is called a window object.
+    //    A single window of a browser can have multiple tabs opened.
+    // 2. Every single tab opened in browser's window has its own independent window object
+    //    and each tab has its own webpage (HTML Document) and its own independant DOM.
+    // 3. This DOM of a tab can be completely manipulated by javascript,
+    //    but to do this javascript needs help of that tab's window object.
+    // 4. Window is a global variable.
+    // 5. Window provide a lot of funtions and tools which can be
+    //    used by javascript.
+
+
+// we can access DOM using "document" keyword. 
+document.querySelector()
+
+// we can access window object of a opened tab using "window" keyword.
+window.scrollX()
